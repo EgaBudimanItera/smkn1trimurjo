db2acf825f2b3c1dadc05dc8aa8b6b5626fbb322
unknown <ega.budiman@staff.itera.ac.id>
2019-07-31 10:01:18 +0700

    2nd commit

--- application/views/dashboard/dashboard.php
+++ application/views/dashboard/dashboard.php
@@ -22,6 +22,8 @@
 <div id="chartContainer" style="height: 370px; width: 100%;"></div>
 <br>
 <div id="chartContainer2" style="height: 370px; width: 100%;"></div>
+<br>
+<div id="chartContainer3" style="height: 370px; width: 100%;"></div>
 <hr>
 <script>
 window.onload = function () {
@@ -85,5 +87,48 @@ window.onload = function () {
 		}]
 	});
 	chart2.render();
+
+	var chart3 = new CanvasJS.Chart("chartContainer3", {
+		animationEnabled: true,
+		
+		title:{
+			text:"Fortune 500 Companies by Country"
+		},
+		axisX:{
+			interval: 1
+		},
+		axisY2:{
+			interlacedColor: "rgba(1,77,101,.2)",
+			gridColor: "rgba(1,77,101,.1)",
+			title: "Number of Companies"
+		},
+		data: [{
+			type: "bar",
+			name: "companies",
+			axisYType: "secondary",
+			color: "#014D65",
+			dataPoints: [
+				{ y: 3, label: "Sweden" },
+				{ y: 7, label: "Taiwan" },
+				{ y: 5, label: "Russia" },
+				{ y: 9, label: "Spain" },
+				{ y: 7, label: "Brazil" },
+				{ y: 7, label: "India" },
+				{ y: 9, label: "Italy" },
+				{ y: 8, label: "Australia" },
+				{ y: 11, label: "Canada" },
+				{ y: 15, label: "South Korea" },
+				{ y: 12, label: "Netherlands" },
+				{ y: 15, label: "Switzerland" },
+				{ y: 25, label: "Britain" },
+				{ y: 28, label: "Germany" },
+				{ y: 29, label: "France" },
+				{ y: 52, label: "Japan" },
+				{ y: 103, label: "China" },
+				{ y: 134, label: "US" }
+			]
+		}]
+	});
+	chart3.render();
 }
 </script>
